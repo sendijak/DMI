1f9d4cceb6915c3da23cfc67bcbd3eb5bdfcedf5
roberts.mukans@gmail.com <sendijak@inbox.lv>
2017-11-09 17:24:25 +0200

    20171109_17_30

---
+++ PYTHON/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+print "Tev ir gulbja sirds."
+print "To \nman \nteica \nkāds \nbērns. "
+print "Mazs\vun \vsirdī \vtik \vtīrs. "
+print "Kā\vZiemassvētku \vrīts ."
+print "Atnes\vman \vlūdzu \vmīļu \vlācīti. "
+print "Ko\vsamīļot \v,kad \vir \vskumji. "
+print "Es\nzinu \ntu \nsarunājies."
+print "Ar\nEnģeļiem \njo \nTev \nir \ngulbja \nsirds. "
---
+++ PYTHON/variables.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#http://www.cplusplus.com/reference/cstdio/printf/?kw=printf
+
+a = 65
+print type(a)
+print "Mainīgā a vērtība kā dec skaitlis: %d"%(a)
+print "Mainīgā a vērtība kā hex skaitlis: %x"%(a)
+print "Mainīgā a vērtība kā oct skaitlis: %o"%(a)
+print "Mainīgā a vērtība kā simbols: %c"%(a)
+
+a = 'A'
+print type(a)
+print "Mainīgā a vērtība kā dec skaitlis: %d"%(ord(a))
+print "Mainīgā a vērtība kā hex skaitlis: %x"%(ord(a))
+print "Mainīgā a vērtība kā oct skaitlis: %o"%(ord(a))
+print "Mainīgā a vērtība kā simbols: %c"%(a)
+
